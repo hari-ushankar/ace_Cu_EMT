1f240ed2a7ceb78e346e5051a712906267054380
humashankar <71093689+hari-ushankar@users.noreply.github.com>
2023-01-30 18:24:45 -0800

    testing pressure units
    add files to support change of pressure units

--- 03-2-use-LAMMPS-with-trained-pot/extract_data.py
+++ 03-2-use-LAMMPS-with-trained-pot/extract_data.py
@@ -27,7 +27,10 @@ energy = lmp.get("PotEng")[-1]
 energy = np.array(energy)
 print(energy)
 ## need to check on the units! I guess LAMMPS deals with atomic style units; but what does ASE use?
-stress = np.array([pxx, pyy, pzz, pyz, pxz, pxy])
+## LAMMPS uses bar for pressure and ASE needs to convert it to eV/ang3!
+stress = np.array([pxx, pyy, pzz, pyz, pxz, pxy]) / 10000 # 1 bar to 1 GPa: https://docs.lammps.org/units.html
+stress = stress / 160.21 ## go from 1 GPa to 1 eV/ang3: http://greif.geo.berkeley.edu/~driver/conversions.html
+stress = -stress #we go from pressure to stress (negative of pressure = stress)
 ## virial's array ordering based on Voigt notation:
 ## https://gitlab.com/ase/ase/-/blob/3.22.1/ase/io/extxyz.py#L519
 ## assign config_type and virial to the atoms
